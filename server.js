4f49b5b2a65dbb6b8e3b69f18f1b7cad3d1e3866
Imran Barbhuiya <74945038+imranbarbhuiya@users.noreply.github.com>
2021-09-09 00:48:39 +0530

    pain

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose.connect(process.env.MONGODB_SRV, {
   useUnifiedTopology: true,
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 passport.use(User.createStrategy());
 
