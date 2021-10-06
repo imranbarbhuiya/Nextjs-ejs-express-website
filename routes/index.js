0854a58d74c1c8df0ac3182ac8818f2b85e5fcf9
Imran Barbhuiya <74945038+imranbarbhuiya@users.noreply.github.com>
2021-10-06 19:26:04 +0530

    refered logic changed

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ const route = Router();
  */
 
 route.get("/", function (req, res) {
-  if (req.query.referred) req.session.referred = req.query.referred;
+  if (!req.isAuthenticated())
+    if (req.query.referred) req.session.referred = req.query.referred;
   res.locals.message = req.flash();
   res.render("index", {
     user: req.user ? req.user : null,
