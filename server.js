d933a009a6144a6fcd856b4af898882ece78d4ed
Imran Barbhuiya <74945038+imranbarbhuiya@users.noreply.github.com>
2021-10-07 07:55:41 +0530

    404 revamped

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ if (app.get("env") === "development") {
     res.status(err.status || 500);
     res.render("error", {
       message: err.message,
-      error: err,
+      status: err.status,
     });
   });
 }
@@ -153,7 +153,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error", {
     message: err.message,
-    error: {},
+    status: err.status,
   });
 });
 
