6f8e7c136e68487a0353dc1f4ec77ca0e13d81ff
Imran Barbhuiya <74945038+imranbarbhuiya@users.noreply.github.com>
2021-10-18 19:42:29 +0530

    react page setup with session

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,44 @@
+import type { Request } from "express";
 import type { NextPage } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Link from "next/link";
 
-const About: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { req } = ctx;
+  const { user } = req as Request;
+  if (!user) {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+  const data = JSON.stringify(user);
+
+  return { props: { data } };
+};
+const About: NextPage = ({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  let user: any;
+  if (data) user = JSON.parse(data);
+  else user = null;
   return (
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
+    <>
+      <h1>{user?.email}</h1>
+      <ul>
+        <li>
+          <Link href="/b" as="/a">
+            <a>a</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/a" as="/b">
+            <a>b</a>
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
