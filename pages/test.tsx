a518d1b489a1c8b920c080c6ef52e73c0246d0c9
Imran Barbhuiya <74945038+imranbarbhuiya@users.noreply.github.com>
2021-10-20 19:00:20 +0530

    forbidden removed

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,23 +1,11 @@
-import { Response } from "express";
 import type { NextPage } from "next";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Link from "next/link";
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { res } = ctx;
-  const { locals } = res as Response;
-
-  return { props: { csrf: locals.csrf } };
-};
-
-const Test: NextPage = ({
-  csrf,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Test: NextPage = () => {
   return (
     <>
-      <form action="/login" method="POST">
-        <input type="hidden" name="_csrf" value={csrf} />
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Why you&#39;ve came here? </h1>
+      <Link href="/">Go Back</Link>
     </>
   );
 };
