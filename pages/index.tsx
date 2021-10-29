eb5c26a1f292ba36a9c5eedf6e5906d0c3d5910c
Imran Barbhuiya <74945038+imranbarbhuiya@users.noreply.github.com>
2021-10-29 13:53:55 +0530

    next image test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import type { NextPage } from "next";
 // next components
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { toast } from "react-toastify";
@@ -134,6 +135,9 @@ const Home: NextPage = ({
           </Link>
         </div>
       </div>
+      <span>
+        <Image src="/img/logo.png" alt="logo" height="500" width="500" />
+      </span>
     </>
   );
 };
